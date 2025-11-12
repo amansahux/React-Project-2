5167ded52cae039b7fbd5c024835e53b99a61ea6
Aman Sahu <amansahu1126@gmail.com>
2025-11-12 11:09:55 +0530

    code optimized

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import ReviewContext from "./Components/ReviewCOntext.jsx";
+
 import CardContext from "./Components/CardContext.jsx";
+import ReviewContext from "./Components/ReviewContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <CardContext>
     <ReviewContext>
+     
       <App />
     </ReviewContext>
   </CardContext>
